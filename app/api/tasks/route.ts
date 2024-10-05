01e264d14e907d5f3f1e357839d2c8d057b5b5b5
Mahmud Hasan Sourov <sourov1666@gmail.com>
2024-10-05 20:34:32 +0600

    created api for task creation

---
+++ app/api/tasks/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from "@/prisma/client";
+
+const createTaskSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+export const POST = async (request: NextRequest) => {
+  const body = await request.json();
+  const validation = createTaskSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  }
+
+  const newTask = await prisma.task.create({
+    data: {
+      title: body.title,
+      description: body.description,
+      updatedAt: new Date(),
+    },
+  });
+
+  return NextResponse.json(newTask, { status: 201 });
+};
